bce1b18bcffd8380165471997b6edc3ef6958378
Tyler Huang <thuang00@stuy.edu>
2020-01-13 19:46:07 -0500

    function display board

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 char * row,col = calloc(5, 5);
-int board[10][10], player1Hit[10][10], player2Hit[10][10], mapSize = 10, numSubmarines = 10, numHits = 10;
+int board[10][10], player1Hit[10][10], player2Hit[10][10], boardSize = 10, numSubmarines = 10, numHits = 10;
 
 void clearScreen() { //clears screen
   system("@cls||clear");
@@ -11,14 +11,45 @@ void clearScreen() { //clears screen
 
 void clearBoard() { //clears the board
   int row,col;
-  for (row = 0; row < mapSize; row++)
+  for (row = 0; row < boardSize; row++)
   {
-    for (column = 0; column < mapSize; column++)
+    for (col = 0; col < boardSize; col++)
     {
-      board[line][column] = 0;
-      player1Hits[line][column] = 0;
-      player2Hits[line][column] = 0;
+      board[line][col] = 0;
+      player1Hits[line][col] = 0;
+      player2Hits[line][col] = 0;
     }
+  }
+}
+
+void setSubmarines() { //takes user input as coords and places a submarine there on the board
+  int i;
+  for (i = 0; i < numSubmarines; i++) {
+    int row, col;
+    printf("Enter the line of submarine %i (0-9): ", i);
+    fgets("%d", &row, stdin);
+    printf("Enter the column of submarine %i (0-9): ", i);
+    fgets("%d", &col, stdin);
+    board[row][col] = 1;
+  }
+}
+
+void display() { //shows board and submarines
+  printf("\nCurrent board and game status:\n");
+  int row,col,i;
+  for (row = 0; row < boardSize; row++) {
+    if (row == 0) {
+      for (i = 0; i <= boardSize; i++) {
+        if (i == 0) printf("   ");
+        else printf(" %d ", i - 1);
+      }
+      printf("\n\n");
+    }
+    for (col = 0; col < boardSize; col++) {
+      if (col == 0) printf("%d  ", row);
+      printf(" %d ", board[row][col]);
+    }
+    printf("\n");
   }
 }
 
