f059a0001a67baa181c932bc54f22be617c99990
Tyler Huang <thuang00@stuy.edu>
2020-01-13 19:30:58 -0500

    function to clear the board

--- main.c
+++ main.c
@@ -2,8 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int map[10][10], player1Shots[10][10], player2Shots[10][10], mapSize = 10, submarinesQty = 10, shotsQty = 10;
+char * row,col = calloc(5, 5);
+int board[10][10], player1Hit[10][10], player2Hit[10][10], mapSize = 10, numSubmarines = 10, numHits = 10;
 
-void clrscr() { //clears screen
+void clearScreen() { //clears screen
   system("@cls||clear");
+}
+
+void clearBoard() { //clears the board
+  int row,col;
+  for (row = 0; row < mapSize; row++)
+  {
+    for (column = 0; column < mapSize; column++)
+    {
+      board[line][column] = 0;
+      player1Hits[line][column] = 0;
+      player2Hits[line][column] = 0;
+    }
+  }
+}
+
+int main() {
+  return 0;
 }
